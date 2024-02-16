904e771ea324e4d02bd3c51c734abe2f36d802a4
Anway <anwaykapoor2311@gmail.com>
2024-02-16 23:36:27 +0530

    Time: 9 ms (54.86%), Space: 8.4 MB (37.07%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,17 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length() != t.length()) return false;
-        unordered_map<char, int> mpp;
-        for(char c : s){
-            mpp[c]++;
-        }
-        for(char c : t){
-            mpp[c]--;
-        }
-        for(auto it = mpp.begin(); it != mpp.end(); it++){
-            if(it -> second != 0) return false;
-        }
-        return true;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
     }
 };
